f7670edac3c2df63c890a109965dad7e7e8b985f
Desktop <guilsalomao@gmail.com>
2019-05-29 23:25:47 -0300

    Resolvido problema de quit

--- menu.lua
+++ menu.lua
@@ -1,25 +1,21 @@
 Menu = Object:extend()
 
 function Menu:new(gm)
-    self.gm = gm
-    self.menus = {["play"] = 1, ["quit"] = 2}
-    self.curSelect =  1
+    self.gm = gm -- receve o gameManager, assim tem acesso aos estados do jogo
+    self.menus = {["play"] = 1, ["quit"] = 2} -- lista dos menus 
+    self.curSelect =  1 -- opção atual
 
 end
 
 function Menu:update()
-    --[[if love.keyboard.isDown('up') then 
-        self.curSelect = self.curSelect - 1
-    elseif love.keyboard.isDown('down') then 
-        self.curSelect = self.curSelect + 1
-    end]]
+    
     function love.keypressed(key,scancode,isrepeat)
         if key == 'up' then
             self.curSelect = self.curSelect - 1
         elseif key == 'down' then
             self.curSelect = self.curSelect - 1
         end
-        if key == 'z' then
+        if key == 'z' and gm.curState == 1 then
             if self.curSelect%2 == 1 then
                 gm.curState = 2
             elseif self.curSelect%2 == 0 then
